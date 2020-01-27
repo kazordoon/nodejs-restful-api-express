0c8fbf4a82632f7b6860be99c1302d27117d7122
Felipe Barros <felipetech@tuta.io>
2020-01-26 22:48:49 -0300

    Change HTTP status code

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -16,10 +16,10 @@ module.exports = {
         return res.status(400).json({ error: 'Este curso já existe' });
 
       const course = await Course.create(req.body);
-      return res.json(course);
+      return res.json(course).code(201);
 
     } catch (err) {
-      return res.status(500).json({ error: 'Não foi possível criar um novo curso' });
+      return res.status(400).json({ error: 'Não foi possível criar um novo curso' });
     }
   },
   async update(req, res) {
@@ -32,9 +32,7 @@ module.exports = {
       const course = await Course.findByIdAndUpdate(req.params.id, req.body, { new: true })
       
       if (!course)
-          return res.status(400).json({ error: 'Este curso não existe' });
-
-      
+          return res.status(400).json({ error: 'Este curso não existe' })
       
       return res.json(course);
       
@@ -45,7 +43,7 @@ module.exports = {
   async delete(req, res) {
     try {
       await Course.findByIdAndDelete(req.params.id);
-      return res.sendStatus(200);
+      return res.sendStatus(204);
 
     } catch (err) {
       return res.status(400).json({ error: 'Não foi possível atualizar o curso' });
