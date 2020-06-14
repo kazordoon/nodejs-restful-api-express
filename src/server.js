fff0231022bc2a91a3d5f0539d43a4d0b404282a
Felipe Barros <felipetech@tuta.io>
2020-06-14 14:53:57 -0300

    perf: Add multiple workers

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const app = require('./config/express')();
+const Cluster = require('./utils/Cluster')();
 
-// Database connection
 require('./config/database')(process.env.MONGO_URL);
 
-app.listen(app.get('PORT'), () => {
-  console.log(`Server running on *:${app.get('PORT')}`);
-});
+const cluster = new Cluster();
+
+if (cluster.isMaster) {
+  cluster.fork();
+  cluster.logger();
+} else {
+  app.listen(app.get('PORT'), () => {
+    console.log(`Server running on *:${app.get('PORT')}`);
+  });
+}
---
+++ src/utils/Cluster.js
@@ -0,0 +1,35 @@
+/* eslint-disable class-methods-use-this */
+const os = require('os');
+const cluster = require('cluster');
+
+module.exports = () => {
+  class Cluster {
+    constructor() {
+      this.numCPUs = os.cpus().length;
+      this.isMaster = cluster.isMaster;
+    }
+
+    fork() {
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < this.numCPUs; i++) {
+        cluster.fork();
+      }
+    }
+
+    logger() {
+      cluster.on('online', (worker) => {
+        console.log(`The worker with the PID ${worker.process.pid} is online.`);
+      });
+
+      cluster.on('disconnect', (worker) => {
+        console.warn(
+          `The worker with the PID ${worker.process.pid} has been disconnected.`,
+        );
+        console.warn('Creating a new worker...');
+        cluster.fork();
+      });
+    }
+  }
+
+  return Cluster;
+};
