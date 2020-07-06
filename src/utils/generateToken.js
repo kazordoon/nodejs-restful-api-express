22db3cea17f64e24b29b557b91ae8189e72fe37a
Felipe Barros <felipetech@tuta.io>
2020-07-06 11:14:55 -0300

    refactor: Change the value of the "expiresIn" property to "1d"

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (app) => {
   const generateToken = (params) => (
     jwt.sign(params, process.env.JWT_KEY, {
-      expiresIn: 86400,
+      expiresIn: '1d',
     })
   );
 
