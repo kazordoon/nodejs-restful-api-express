b02aab44f3a75819253624762e6ffb848f6e961a
Felipe Barros <felipetech@tuta.io>
2020-08-24 08:48:10 -0300

    refactor: Add conditions to variables
    Add conditions to variables and use them in the conditional structures

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -15,7 +15,8 @@ module.exports = (app) => {
 
       const { username } = req.body;
 
-      if (await User.findOne({ username })) {
+      const userAlreadyExists = await User.findOne({ username });
+      if (userAlreadyExists) {
         return res.status(409).json({ error: 'This user already exists' });
       }
 
@@ -39,14 +40,17 @@ module.exports = (app) => {
 
       const { username, password } = req.body;
 
-      // The "select" function is used to return the user's password
       const user = await User.findOne({ username }).select('+password');
 
       if (!user) {
         return res.status(400).json({ error: "This user doesn't exist" });
       }
 
-      if (!await bcrypt.compare(password, user.password)) {
+      const isThePasswordIncorrect = !(await bcrypt.compare(
+        password,
+        user.password,
+      ));
+      if (isThePasswordIncorrect) {
         return res.status(401).json({ error: 'Authentication failed' });
       }
 
