e3037116837edc41d162bec22ec4dbc3b9c2cb47
Felipe Barros <felipetech@tuta.io>
2020-10-02 15:18:07 +0000

    refactor: Create copy of a required schema using "reduce"

--- src/app/routes/courseRoutes.js
+++ src/app/routes/courseRoutes.js
@@ -4,10 +4,7 @@ const { courseSchema } = require('../../validators');
 module.exports = (app) => {
   const { coursesController } = app.controllers;
   const { auth } = app.middlewares;
-  const {
-    requiredSchema: checkRequiredSchema,
-    optionalSchema: checkOptionalSchema,
-  } = courseSchema;
+  const { checkRequiredSchema, checkOptionalSchema } = courseSchema;
 
   router.get('/', coursesController.index);
   router.get('/:id', coursesController.getOne);
--- src/validators/courseSchema.js
+++ src/validators/courseSchema.js
@@ -3,7 +3,7 @@ const { checkSchema } = require('express-validator');
 
 const currentYear = new Date().getFullYear();
 
-const requiredSchema = checkSchema({
+const requiredSchema = {
   name: {
     isLength: {
       options: { min: 5, max: 50 },
@@ -34,47 +34,22 @@ const requiredSchema = checkSchema({
     },
     errorMessage: `Course year must be between 1970 and ${currentYear}`,
   },
-});
+};
 
-const optionalSchema = checkSchema({
-  name: {
-    isLength: {
-      options: { min: 5, max: 50 },
-    },
-    errorMessage: 'Course name must be between 5 to 50 characters',
-    optional: true,
+const requiredSchemaAuxiliary = JSON.parse(JSON.stringify(requiredSchema));
+const optionalSchema = Object.entries(requiredSchemaAuxiliary).reduce(
+  (accumulator, [key, value]) => {
+    accumulator[key] = value;
+    accumulator[key].optional = true;
+    return accumulator;
   },
-  description: {
-    isLength: {
-      options: { min: 15, max: 100 },
-    },
-    errorMessage: 'Course description must be 15 to 100 characters',
-    optional: true,
-  },
-  workload: {
-    isInt: {
-      options: { min: 1 },
-    },
-    errorMessage: 'Course workload must be at least 1 hour',
-    optional: true,
-  },
-  total_classes: {
-    isInt: {
-      options: { min: 1 },
-    },
-    errorMessage: 'Course must have at least 1 class',
-    optional: true,
-  },
-  year: {
-    isInt: {
-      options: { min: 1970, max: currentYear },
-    },
-    errorMessage: `Course year must be between 1970 and ${currentYear}`,
-    optional: true,
-  },
-});
+  {},
+);
+
+const checkRequiredSchema = checkSchema(requiredSchema);
+const checkOptionalSchema = checkSchema(optionalSchema);
 
 module.exports = {
-  requiredSchema,
-  optionalSchema,
+  checkRequiredSchema,
+  checkOptionalSchema,
 };
